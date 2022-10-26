b2a214f417806146041b5be8d4b154add5b8b2d5
Hops <jonhops2@gmail.com>
2022-10-25 20:22:45 -0400

    Added stable colors

--- HW04/3DMorph.js
+++ HW04/3DMorph.js
@@ -90,26 +90,17 @@ var h_vertices = [
 ]
 var vertexColors = [
     vec4(1.0, 0.0, 0.0, 1.0), // red
-    vec4(0.0, 0.0, 0.0, 1.0),  // black
     vec4(0.0, 1.0, 0.0, 1.0),  // green
     vec4(0.0, 0.0, 1.0, 1.0),  // blue
     vec4(1.0, 1.0, 0.0, 1.0),  // yellow
     vec4(1.0, 0.0, 1.0, 1.0), // magenta
     //vec4(1.0, 1.0, 1.0, 1.0),  // white
-    //vec4(0.0, 1.0, 1.0, 1.0)   // cyan */
+    vec4(0.0, 1.0, 1.0, 1.0), // cyan 
     vec4(0.0, 0.0, 1.0, 1.0),  // blue
     vec4(1.0, 1.0, 0.0, 1.0),  // yellow
     vec4(1.0, 0.0, 1.0, 1.0), // magenta
     vec4(1.0, 0.0, 0.0, 1.0)
 ];
-
-    var vertexColorNames = [
-        "Red",
-        "Blue",
-        "Yellow",
-        "Magenta"
-    ];
-
 
 init();
 
@@ -133,7 +124,6 @@ function init()
 
     //Call draw function
     drawj();
-
     // vertex array attribute buffer
 
     //J Buffer
@@ -147,7 +137,6 @@ function init()
     gl.enableVertexAttribArray(positionLoc );
 
     //H Buffer
-    //drawh();
 
     var hBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, hBuffer);
@@ -192,45 +181,41 @@ function init()
 }
 
 
-function drawFace(a,b,c,d){
+function drawFace(a,b,c,d,colorCode){
     let indices = [a, b, c, a, c, d];
     for(var i = 0; i < indices.length; i++){
         h_positions.push(h_vertices[indices[i]]);
-        j_positions.push(j_vertices[indices[i]])
-        colors.push(vertexColors[indices[0] % vertexColors.length]);
+        j_positions.push(j_vertices[indices[i]]);
+        colors.push(vertexColors[colorCode]);
         //colors.push(vec4(1.0, 0.0, 0.0, 1.0));
     }
 }
 
 function drawj(){
 
-    //positions = []; //Reset positions
-    colors = []; //Reset colors
-
-
     //Bottom of J
-    drawFace(0, 1, 2, 3); //Left bottom
-    drawFace(0, 4, 5, 1); //Front bottom
-    drawFace(3, 2, 7, 6); //Back bottom
-    drawFace(0, 4, 6, 3); //Bottom bottom
-    drawFace(2, 1, 5, 7); //Top bottom
-    drawFace(4, 6, 7, 5);//Right bottom
+    drawFace(0, 1, 2, 3, 0); //Left bottom
+    drawFace(0, 4, 5, 1,1); //Front bottom
+    drawFace(3, 2, 7, 6,2); //Back bottom
+    drawFace(0, 4, 6, 3,3); //Bottom bottom
+    drawFace(2, 1, 5, 7,4); //Top bottom
+    drawFace(4, 6, 7, 5,5);//Right bottom
     
     //Middle of J
-    drawFace(8,9,10,11); //Front middle
-    drawFace(9,13,14,10);  //Right middle
-    drawFace(12,13,14,15);   //Back middle
-    drawFace(12,15,11,8);   //Left middle
-    drawFace(15,14,10,11); //Top of middle
-    drawFace(8,9,13,12); //Bottom of middle
+    drawFace(8,9,10,11,1); //Front middle
+    drawFace(9,13,14,10,5);  //Right middle
+    drawFace(12,13,14,15,2);   //Back middle
+    drawFace(12,15,11,8,0);   //Left middle
+    drawFace(15,14,10,11,4); //Top of middle
+    drawFace(8,9,13,12,3,5); //Bottom of middle
 
     //Top of J
-    drawFace(16,17,18,19); //Front top
-    drawFace(17,21,22,18); //Right top
-    drawFace(20,21,22,23); //Back top
-    drawFace(20,16,19,23);  //Left top
-    drawFace(18,22,23,19); //Top Top
-    drawFace(17,21,20,16);   //Bottom top
+    drawFace(16,17,18,19,1); //Front top
+    drawFace(17,21,22,18,5); //Right top
+    drawFace(20,21,22,23,2); //Back top
+    drawFace(20,16,19,23,0);  //Left top
+    drawFace(18,22,23,19,4); //Top Top
+    drawFace(17,21,20,16,3);   //Bottom top
 
     
 }
@@ -272,9 +257,9 @@ function render()
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         //Change direction of morph
-    if(morphPoint >= 1.0)  morphBy = -0.010;
+    if(morphPoint >= 1.0)  morphBy = -0.007;
 
-    else if (morphPoint <= 0.0) morphBy = 0.010;
+    else if (morphPoint <= 0.0) morphBy = 0.007;
 
     morphPoint += (morphToggle ? morphBy : 0.0);
 
